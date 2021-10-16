e4125fcae557ba59721c028d8e7910d1a240440f
ShreyankMehra <89086034+ShreyankMehra@users.noreply.github.com>
2021-10-16 19:10:47 +0530

    Add files via upload

---
+++ options.py
@@ -0,0 +1,15 @@
+def display():
+    print('''\n1. Show the seats
+2. Buy a Ticket
+3. Statistics
+4. Show booked Tickets User Info
+0. Exit
+''')
+
+
+
+
+
+
+
+
---
+++ testseatbookingview.py
@@ -0,0 +1,17 @@
+
+'''ticket_no=34
+for i in range(1,6):
+    for j in range(1,6):
+        no=int(str(i)+str(j))
+        if no==ticket_no:
+            print('b',end='')
+        else:
+            print('s',end='')
+    print('\n',end='')
+
+
+def pricing(rows,seats,booked):'''
+
+
+r=int(input())
+print(r)
